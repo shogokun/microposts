1106af5b866e2d51393ab86bdf33a75cccb5690e
Shogo Kitazume <beaver.dubmaster@gmail.com>
2016-11-25 03:39:58 +0000

    Gemfile update for heroku

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,7 @@ class UsersController < ApplicationController
   
   def show
     @user = User.find(params[:id])
-    @microposts = @user.microposts.order(created_at: :desc)
+    @microposts = @user.microposts.order(created_at: :desc).page(params[:page])
   end
   
   def new
@@ -37,14 +37,14 @@ class UsersController < ApplicationController
   def followings
     @title = 'followings'
     @user = User.find(params[:id])
-    @users = @user.following_users
+    @users = @user.following_users.page(params[:page])
     render 'show_follow'
   end
   
   def followers
     @title = 'followers'
     @user = User.find(params[:id])
-    @users = @user.follower_users
+    @users = @user.follower_users.page(params[:page])
     render 'show_follow'
   end
   
